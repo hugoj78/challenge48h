18580081b241e998e2661c5891b9d950fef1146a
NeeaaR <alexis.morin77@yahoo.fr>
2020-02-11 10:04:47 +0100

    views

--- gloth/project/views.py
+++ gloth/project/views.py
@@ -1,13 +1,20 @@
 import os
 from werkzeug import secure_filename
-from flask import Flask, request, redirect, url_for, render_template, flash
+from flask import Flask, request, redirect, url_for, render_template, flash, session
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+
 
 app = Flask(__name__)
 app.config.from_object("project.config.Config")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
 
 from .forms import PatientForm, MedicForm
 
+from .models import *
 @app.route("/", methods=["GET", "POST"])
+
 @app.route("/index", methods=["GET", "POST"])
 def index():
     form = PatientForm()
@@ -18,17 +25,23 @@ def index():
             return render_template("index.html", title="Gloth", subtitle="test", patient_form=form, name="Ynov")
         else:
             data = request.args.get(form)
-            return redirect(url_for('medic'), data=data)
+            return redirect(url_for('select'), data=data)
 
     return render_template("index.html", title="Gloth", subtitle="subtitle", patient_form=form, name="Ynov")
 
+@app.route('/select', methods=["GET", "POST"])
+def select():
 
-@app.route('/medic', methods=["GET","POST"])
-def medic():
+    return render_template("select.html", name="Ynov")
 
-    form = MedicForm(request.form)
-    patho_id = (request.form.get("pathology"))
-    user_id = (request.form.get("user"))
-    return render_template("medic.html", name="Ynov", pathology=patho_id, user=user_id)
+@app.route('/ordonnance', methods=["GET"])
+def posology():
+
+    return render_template("ordonnance.html", name="Ynov")
+
+@app.route('/medicaments', methods=["GET"])
+def medicaments():
+
+    return render_template("medicaments.html", name="Ynov")
 
 	
